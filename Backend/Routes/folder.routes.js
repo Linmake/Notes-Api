3c153f8da865b3d428e561faa0309e7b50dea948
linmake <lin.web.05@gmail.com>
2025-09-26 14:30:19 -0700

    dot error

--- Backend/Routes/folder.routes.js
+++ Backend/Routes/folder.routes.js
@@ -3,7 +3,7 @@ import express from "express";
 import createController from '../Controllers/Folder/Create.controller.js';
 import deleteController from '../Controllers/Folder/delete.controller.js';
 import deleteAllOnProjectController from '../Controllers/Folder/deleteAllOnProject.controller.js';
-import editController from '../Controllers/Folder/edit.controller..js';
+import editController from '../Controllers/Folder/edit.controller.js';
 import deleteAllController from '../Controllers/Folder/deleteAll.controller.js';
 import getAllOnProjectController from '../Controllers/Folder/getAllOnProject.controller.js';
 import getAllFoldersController from '../Controllers/Folder/getAllFolders.controller.js';
