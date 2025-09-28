55a1f62a2794da01e48fc1db03d03a8660a3c7ba
linmake <lin.web.05@gmail.com>
2025-09-27 22:26:26 -0700

    actually

--- Backend/Routes/assistant.routes.js
+++ Backend/Routes/assistant.routes.js
@@ -1,6 +1,5 @@
 import express from "express";
-import { resumeAssistantController } from '../src/Controllers/Assitant/resume.controller.js';
-import { testController } from "../src/Controllers/Assitant/test.controller.js";
+import { resumeAssistantController } from '../Controllers/Assitant/resume.controller.js';
 
 const AssitantRouter = express.Router(); 
 
