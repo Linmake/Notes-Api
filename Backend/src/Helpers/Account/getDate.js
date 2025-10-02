c5aac663b1b1e2181617932394d690e2d25fec60
linmake <lin.web.05@gmail.com>
2025-10-02 06:24:01 -0700

    Refactoring: remove backend from fontend

--- Backend/src/Helpers/Account/getDate.js
+++
@@ -1,6 +0,0 @@
-import { format } from "date-fns";
-
-/**Fecha actual en formato: 
- * yyyy-MM-dd
- */
-export const getDate = format(new Date(), "yyyy-MM-dd")
--- Backend/src/server.js
+++
@@ -1,7 +0,0 @@
-import App from './lambda.js';
-
-const PORT = 3000; // valor fijo
-
-App.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-});
